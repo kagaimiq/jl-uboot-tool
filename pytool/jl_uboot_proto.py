519be8016ad2d739466f866331b250108bf26438
Andrey Grigoryev <andrey-grigorev-2006@yandex.ru>
2022-08-29 21:54:19 +0300

    moarr stuff - BR25 stuff!

--- pytool/jl_uboot_proto.py
+++ pytool/jl_uboot_proto.py
@@ -1,4 +1,4 @@
-import os, fcntl, ctypes
+import os, fcntl, ctypes, time
 import crcmod
 import sys
 
@@ -82,33 +82,111 @@ class SCSI():
 
         fcntl.ioctl(self.fd, SCSI.SG_IO, sgio)
 
+################################################################################
+
 jl_crc16 = crcmod.mkCrcFun(0x11021, rev=False, initCrc=0x0000, xorOut=0x0000)
 
-with SCSI(sys.argv[1]) as scsi:
-    def hexdump(data, width=16):
-        for i in range(0, len(data), width):
-            s = '%8X: ' % i
+def jl_crypt(data, key=0xffff):
+    data = bytearray(data)
 
-            for j in range(width):
-                if (i+j) < len(data):
-                    s += '%02X ' % data[i+j]
-                else:
-                    s += '-- '
+    for i in range(len(data)):
+        data[i] ^= key & 0xff
+        key = ((key << 1) ^ (0x1021 if (key >> 15) else 0)) & 0xffff
 
-            s += ' |'
+    return bytes(data)
 
-            for j in range(width):
-                if (i+j) < len(data):
-                    c = data[i+j]
-                    if c < 0x20 or c >= 0x7f: c = ord('.')
-                    s += chr(c)
-                else:
-                    s += ' '
+def jl_cryptcrc(data, key=0xffffffff):
+    crc = key & 0xffff
+    crc = jl_crc16(bytes([key >> 16 & 0xff, key >> 24 & 0xff]), crc)
 
-            s += '|'
+    magic = b'\xc3\xcf\xc0\xe8\xce\xd2\xb0\xae\xc4\xe3\xa3\xac\xd3\xf1\xc1\xd6'
 
-            print(s)
-        print()
+    data = bytearray(data)
+
+    for i in range(len(data)):
+        crc = jl_crc16(bytes([magic[i % len(magic)]]), crc)
+        data[i] ^= crc & 0xff
+
+    return bytes(data)
+
+
+def hexdump(data, width=16):
+    for i in range(0, len(data), width):
+        s = '%8X: ' % i
+
+        for j in range(width):
+            if (i+j) < len(data):
+                s += '%02X ' % data[i+j]
+            else:
+                s += '-- '
+
+        s += ' |'
+
+        for j in range(width):
+            if (i+j) < len(data):
+                c = data[i+j]
+                if c < 0x20 or c >= 0x7f: c = ord('.')
+                s += chr(c)
+            else:
+                s += ' '
+
+        s += '|'
+
+        print(s)
+    print()
+
+#######################
+
+scsi = None
+
+for ntry in range(2, -1, -1):
+    print("Waiting for `%s`" % sys.argv[1], end='', flush=True)
+
+    while True:
+        print(".", end='', flush=True)
+
+        try:
+            scsi = SCSI(sys.argv[1])
+
+            print("connected")
+            break
+
+        except FileNotFoundError:
+            pass
+
+        except Exception as e:
+            print("failed\n", e)
+            exit(1)
+
+        time.sleep(0.5)
+
+    inquiry = scsi.xfer_fromdev(b'\x12\x00\x00\x00\x24\x00', 0x24)
+    print('Inquiry: [%s] [%s] [%s]' % (str(inquiry[8:16], 'ascii'),
+                                       str(inquiry[16:32], 'ascii'),
+                                       str(inquiry[32:36], 'ascii')))
+
+    if inquiry[16:21] == b'UBOOT':
+        break
+
+    else:
+        if ntry > 0:
+            print("This is not an UBOOT device yet, try to enter...")
+
+            # reset cmd
+            scsi.xfer_fromdev(b'\xfc\x0c\x00\x00\x00\x01', 16)
+
+        else:
+            print("This is not an JieLi device!")
+            exit(1)
+
+    scsi.close()
+
+    time.sleep(.5)
+
+################################################################################
+
+with scsi:
+    print("Kagamii")
 
     ###############################################################################################
 
@@ -134,12 +212,25 @@ with SCSI(sys.argv[1]) as scsi:
         res = scsi.xfer_fromdev(b'\xfb\x08' + addr.to_bytes(4, 'big') + arg.to_bytes(2, 'big'), 16)
         assert(res[:2] == b'\xfb\x08')
 
-    '''
-    with open(sys.argv[2], 'rb') as f:
-        JL_mem_write(0x2000, f.read())
-        JL_mem_call(0x2000)
-    '''
 
+
+    #https://nitter.42l.fr/whitequark/status/1139284289105924097
+    #https://cloud.whitequark.org/s/WPMSZM7CrbT6Gde/download
+
+
+    with open(sys.argv[2], 'rb') as f:
+        JL_mem_write(0x12000, jl_cryptcrc(f.read()))
+        JL_mem_call(0x12000)
+
+        data = jl_cryptcrc(JL_mem_read(0x20000, 0x4000))
+        sys.stdout.buffer.write(data[2:2+int.from_bytes(data[:2], 'little')])
+
+
+    #-------------------------------------------------------
+
+
+
+    '''
     with open('br21loader.bin', 'rb') as f:
         JL_mem_write(0x2000, f.read())
         JL_mem_call(0x2000, 1)
@@ -148,3 +239,14 @@ with SCSI(sys.argv[1]) as scsi:
     #JL_flash_write(0x20, b'<Kagami Hiiragi>')
     #JL_flash_write(0x40, b'<<Konata Izumi>>')
     hexdump(JL_flash_read(0, 0x100))
+
+    with open(sys.argv[2], 'rb') as f:
+        i = 0
+        while True:
+            dat = f.read(4096)
+            if len(dat) == 0: break
+            print('%06x' % i)
+            JL_flash_eraseSector(i)
+            JL_flash_write(i, dat)
+            i += 4096
+    '''
