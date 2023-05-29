f2b05808c2a9c410074fb888a3d2a2ef545a2da1
Andrey Grigoryev <andrey-grigorev-2006@yandex.ru>
2023-05-30 01:08:52 +0300

    a bit of refactoring on jldevfind

--- jldevfind.py
+++ jldevfind.py
@@ -1,30 +1,60 @@
 from scsiio import SCSIDev
-import sys
+from scsiio.common import SCSIException
+import sys, os
 
 def find_jl_devices():
+    devpaths = []
+
+    if sys.platform == 'linux':
+        #
+        # FIXME - hardcoded '/dev' probably isn't right..
+        #
+        devdir = '/dev'
+
+        for path in os.listdir(devdir):
+            if path.startswith('sg'):
+                devpaths.append(os.path.join(devdir, path))
+
+    elif sys.platform == 'win32':
+        #
+        # XXX FIXME TODO - proper way of finding that 
+        #    also it limits to 128 volumes which won't work
+        #    right after it exceeds 127.
+        #    (as the drives doesn't occupy the same number)
+        #
+
+        #for i in range(26):
+        #    path = '\\\\.\\%s:' % chr(ord('A') + i)
+        #    if os.path.exists(path):
+        #        devpaths.append(path)
+
+        for i in range(128):
+            path = '\\\\.\\HardDiskVolume%d' % i
+            if os.path.exists(path):
+                devpaths.append(path)
+    else:
+        raise NotImplemented('Platform %s is not supported (yet)' % sys.platform)
+
     devs = []
 
-    for i in range(128): # 128 may be enough?...
-        if sys.platform == 'linux':
-            path = '/dev/sg%d' % i
-        elif sys.platform == 'win32':
-            path = '\\\\.\\HardDiskVolume%d' % i
-        else:
-            raise NotImplemented('Platform %s is not supported (yet)' % sys.platform)
-
+    for path in devpaths:
         try:
             with SCSIDev(path) as dev:
-                data = bytearray(36)
-                dev.execute(b'\x12' + b'\x00\x00' + len(data).to_bytes(2, 'big')
-                                                       + b'\x00', None, data)
+                inquiry = bytearray(36)
+                dev.execute(b'\x12' + b'\x00\x00' + len(inquiry).to_bytes(2, 'big') + b'\x00', None, inquiry)
 
-                vendor = str(data[8:16], 'ascii').strip()
-                product = str(data[16:32], 'ascii').strip()
-                prodrev = str(data[32:36], 'ascii').strip()
+                vendor   = str(inquiry[ 8:16], 'ascii').strip()
+                product  = str(inquiry[16:32], 'ascii').strip()
+                revision = str(inquiry[32:36], 'ascii').strip()
 
+                # TODO: filter based on the 'vendor', not on 'product', we don't want any wrong false-positive
                 if product.startswith(('UBOOT', 'UDISK', 'DEVICE')):
-                    devs.append({'path': path, 'name': '%s %s (%s) at %s' % (vendor, product, prodrev, path)})
-        except:
+                    devs.append({'path': path, 'name': '%s %s (%s) at %s' % (vendor, product, revision, path)})
+
+        except SCSIException:
+            pass
+
+        except PermissionError:
             pass
 
     return devs
