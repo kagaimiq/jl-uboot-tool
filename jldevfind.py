b80fc0b41e798cd7a620c13b738f711d6dcefd44
Andrey Grigoryev <andrey-grigorev-2006@yandex.ru>
2023-12-05 07:27:22 +0300

    devfind stuff and waiting for a device up there

--- jldevfind.py
+++ jldevfind.py
@@ -1,8 +1,9 @@
 from scsiio import SCSIDev
 from scsiio.common import SCSIException
-import sys, os
+import sys, os, time
+import subprocess, re
 
-def find_jl_devices():
+def find_jl_devices(venfilter=None):
     devpaths = []
 
     if sys.platform == 'linux':
@@ -32,6 +33,22 @@ def find_jl_devices():
             path = '\\\\.\\HardDiskVolume%d' % i
             if os.path.exists(path):
                 devpaths.append(path)
+
+        # stolen from UF2 utility thing
+        # yet it doesn't really work now
+        '''
+        #res = subprocess.check_output(['wmic', 'DISKDRIVE', 'get', 'DeviceID,InterfaceType']).decode('ascii')
+        res = subprocess.check_output(['wmic', 'DISKDRIVE', 'get', 'DeviceID,InterfaceType']).decode('ascii')
+        print(res)
+
+        for line in res.split('\n'):
+            words = re.split('\s+', line)
+            if len(words) < 2: continue
+            print(words)
+            if words[1] == 'USB':
+                devpaths.append(words[0])
+        '''
+
     else:
         raise NotImplemented('Platform %s is not supported (yet)' % sys.platform)
 
@@ -47,6 +64,10 @@ def find_jl_devices():
                 product  = str(inquiry[16:32], 'ascii').strip()
                 revision = str(inquiry[32:36], 'ascii').strip()
 
+                if venfilter is not None:
+                    if vendor.lower() != venfilter.lower():
+                        continue
+
                 # TODO: filter based on the 'vendor', not on 'product', we don't want any wrong false-positive
                 if product.startswith(('UBOOT', 'UDISK', 'DEVICE')):
                     devs.append({'path': path, 'name': '%s %s (%s) at %s' % (vendor, product, revision, path)})
@@ -59,8 +80,33 @@ def find_jl_devices():
 
     return devs
 
-def choose_jl_device():
-    devs = find_jl_devices()
+def choose_jl_device(venfilter=None, wait=True):
+    att = 0
+
+    if venfilter is not None:
+        print('Searching for "%s" devices..' % venfilter)
+    else:
+        print('Searching for some JieLi devices..')
+
+    while True:
+        devs = find_jl_devices(venfilter)
+
+        if not wait:
+            break
+
+        else:
+            if len(devs) > 0:
+                if att > 0:
+                    print(' - got one!')
+                break
+
+            att += 1
+
+            print('\rNo devices found yet. Attempt %d' % att, end='', flush=True)
+
+        time.sleep(1)
+
+    ## ---------------------------------------------- ##
 
     if len(devs) == 0:
         print('No devices found')
